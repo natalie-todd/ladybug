68792ed23d998ae2aa47d8a724fb356ab5ceba6e
natalie-todd <natalie.todd15@gmail.com>
2020-03-01 15:35:46 -0700

    Add head and antennae

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+                        <span className="antennae-dot-container">
+                <span className="antennae-dot"/>
+                <span className="antennae-dot"/>
+            </span>
+            <span className="antennae-container">
+                <span className="antennae"/>
+                <span className="antennae"/>
+            </span>
+            <span className="bug-head"/>
+            <span className="bug-body">
+              <span className="side">
+                  <span className="spot"/>
+                  <span className="spot"/>
+                  <span className="spot"/>
+              </span>
+              <span className="vertical-line"/>
+              <span className="side">
+                  <span className="spot"/>
+                  <span className="spot"/>
+                  <span className="spot"/>
+              </span>
+          </span>
+        </div>
+    );
 }
 
 export default App;
