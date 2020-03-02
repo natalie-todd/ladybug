331939ee28297ba457445b430dc7aadfe8d48a54
natalie-todd <natalie.todd15@gmail.com>
2020-03-01 17:27:27 -0700

    Add legs

--- src/App.js
+++ src/App.js
@@ -4,28 +4,40 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-                        <span className="antennae-dot-container">
-                <span className="antennae-dot"/>
-                <span className="antennae-dot"/>
+            <span className="antennae-dot-container">
+                <div className="antennae-dot"/>
+                <div className="antennae-dot"/>
             </span>
             <span className="antennae-container">
-                <span className="antennae"/>
-                <span className="antennae"/>
+                <div className="antennae"/>
+                <div className="antennae"/>
             </span>
             <span className="bug-head"/>
+            <div className="body-space">
+                <span className="leg-space">
+                    <div className="left-leg"/>
+                    <div className="left-leg"/>
+                    <div className="left-leg"/>
+                </span>
             <span className="bug-body">
-              <span className="side">
+              <div className="side">
                   <span className="spot"/>
                   <span className="spot"/>
                   <span className="spot"/>
-              </span>
-              <span className="vertical-line"/>
-              <span className="side">
+              </div>
+              <div className="vertical-line"/>
+              <div className="side">
                   <span className="spot"/>
                   <span className="spot"/>
                   <span className="spot"/>
-              </span>
-          </span>
+              </div>
+            </span>
+                <span className="leg-space">
+                    <div className="right-leg"/>
+                    <div className="right-leg"/>
+                    <div className="right-leg"/>
+                </span>
+          </div>
         </div>
     );
 }
