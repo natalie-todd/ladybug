d83899f5352abb5fc9515332f8615f000e9d660e
natalie-todd <natalie.todd15@gmail.com>
2020-03-06 22:09:02 -0700

    Refactor dice logic and style h1

--- src/App.js
+++ src/App.js
@@ -12,15 +12,23 @@ function App() {
     const [total, setTotal] = useState(6);
     return (
         <div className="App">
+            <h1>
+                Ladybug Dice
+            </h1>
             {getBodyParts("antennae-dot", 2)}
             {getBodyParts("antennae", 2)}
             <span className="bug-head"/>
             <div className="body-space" onClick={() => setTotal(Math.floor((Math.random() * 6) + 1))}>
                 {getBodyParts("left-leg", 3)}
                 <span className="bug-body">
-                {getBodyParts("spot", total <= 3 ? total : 3)}
+                {getBodyParts("spot", total > 4 ? 3
+                    : total > 2 ? 2
+                        : 1)}
                     <div className="vertical-line"/>
-                    {getBodyParts("spot", total > 3 ? total -3 : 0)}
+                    {getBodyParts("spot", total === 6 ? 3
+                        : total > 3 ? 2
+                            : total > 1 ? 1
+                                : 0)}
             </span>
                 {getBodyParts("right-leg", 3)}
             </div>
